e2499583eaabc7d1ace64e86df7611fb7cbb34b7
Epoggi <11327084+Epoggi@users.noreply.github.com>
2021-08-04 15:33:37 +0300

    Lists ready, setting up FixedSizeList

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import { useState, useEffect } from 'react';
 import './App.css';
+import Grid from '@material-ui/core/Grid';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import { FixedSizeList } from 'react-window';
 
 function parseRules(data) {
   const regexRules = /^(\d+\.\d+[\w]?[\.]?)\s(.*)/gmi;
   var matches = [];
   var match = regexRules.exec(data);
   while (match != null) {
-    matches.push({ rule: match[1], text: match[2] });
+    matches.push({ number: match[1], text: match[2] });
     match = regexRules.exec(data);
   }
   return matches;
-  debugger
 }
 function parseChapters(data) {
 const regexChapters = /^\r\n(\d+)\.\s(.*)\r\n$/gm;
 var matches = [];
 var match = regexChapters.exec(data);
 while (match != null) {
-  matches.push({ number: match[1], title: match[2]});
+  matches.push({ number: match[1], text: match[2]});
   match = regexChapters.exec(data);
 }
 return matches
-debugger
 }
 async function fetchData() {
   
@@ -34,6 +37,8 @@ function App() {
   const [isLoaded, setIsLoaded] = useState(false);
   const [chapters, setChapters] = useState([]);
   const [rules, setRules] = useState([]);
+  const [mixed, setMixed] = useState([]);
+
   //Fetch the rules.txt need to ask permission at "https://cors-anywhere.herokuapp.com/corsdemo"
   useEffect(() => {
     fetch(`https://cors-anywhere.herokuapp.com/https://media.wizards.com/2021/downloads/MagicCompRules%2020210419.txt`)
@@ -51,29 +56,42 @@ function App() {
         }
       )
       //After states are filled, mix the chapters and rules into a single list
-      .then(chaptersAndRules(chapters, rules))
   }, [])
+
+  useEffect(() => {
+    if (chapters.length > 0) {
+      chaptersAndRules(chapters, rules)
+    }
+
+  }, [chapters, rules])
 
   const chaptersAndRules = (chapters, rules) => {
     console.log("first chapter: " + chapters[0])
     console.log("first rule: " + rules[0])
     var mixed = [];
-  debugger
     for (var i = 0; i < chapters.length; i++) {
       mixed.push(chapters[i])
   
-      for (var i2 = 0; i < rules.length; i2++){
-        console.log(rules[i2])
+      for (var i2 = 0; i2 < rules.length; i2++){
         
-        if (chapters[i].number === rules[i2].rule.slice(0,3)) {
+        if (chapters[i].number == rules[i2].number.slice(0,3)) {
           mixed.push(rules[i2])
         }
       }
     }
-    console.log("logging mixed list:" + mixed)
-    debugger
+    setMixed(mixed)
   }
+
   
+  function renderRow(props) {
+    const { number, text } = props;
+
+    return (
+      <ListItem button key={number}>
+        <ListItemText primary={number + " " + text}/>
+      </ListItem>
+    );
+  }
 
   //Possible returns
   if (error) {
@@ -83,22 +101,31 @@ function App() {
   } else {
     return (
       <div>
+        <FixedSizeList height={300} width={200} itemSize={30} itemCount={10}>
+          {renderRow}
+        </FixedSizeList>
+        <Grid container spacing={3}>
+          <Grid item xs={6}>
         <p>Chapters</p>
-        <ul>
-          {chapters.map(item=> (
-            <li key={item.number}>
-              {item.number} {item.title}
-            </li>
+        <List>
+      {chapters.map(item=> (
+            <ListItem key={item.number}>
+              {item.number} {item.text}
+            </ListItem>
           ))}
-        </ul>
+        </List>
+          </Grid>
+          <Grid item xs={6}>
         <p>Rules</p>
-        <ul>
-          {rules.map(item => (
-            <li key={item.rule}>
-              {item.rule} {item.text}
-            </li>
+        <List>
+      {mixed.map(item=> (
+            <ListItem key={item.number}>
+              {item.number} {item.text}
+            </ListItem>
           ))}
-        </ul>
+        </List>
+          </Grid>
+        </Grid>
       </div>
     )
   }
