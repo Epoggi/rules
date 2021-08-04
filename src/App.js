553dff011479b1db6e626024a54c3887690cc7e6
Epoggi <11327084+Epoggi@users.noreply.github.com>
2021-08-04 10:50:16 +0300

    latest problem

--- src/App.js
+++ src/App.js
@@ -13,7 +13,18 @@ function parseRules(data) {
   debugger
 }
 function parseChapters(data) {
-
+const regexChapters = /^\r\n(\d+)\.\s(.*)\r\n$/gm;
+var matches = [];
+var match = regexChapters.exec(data);
+while (match != null) {
+  matches.push({ number: match[1], title: match[2]});
+  match = regexChapters.exec(data);
+}
+return matches
+debugger
+}
+async function fetchData() {
+  
 }
 
 
@@ -23,29 +34,46 @@ function App() {
   const [isLoaded, setIsLoaded] = useState(false);
   const [chapters, setChapters] = useState([]);
   const [rules, setRules] = useState([]);
-  const [data, setData] = useState('');
   //Fetch the rules.txt need to ask permission at "https://cors-anywhere.herokuapp.com/corsdemo"
   useEffect(() => {
     fetch(`https://cors-anywhere.herokuapp.com/https://media.wizards.com/2021/downloads/MagicCompRules%2020210419.txt`)
       .then(res => res.text())
       .then(
-        (data) => {
+        (fetchData) => {
           setIsLoaded(true);
-          //set full data into a state, maybe remove this part
-          setData(data);
-          //rules object is created through parseRules function which parses data into items containing rules and texts
-          setRules(parseRules(data));
-          console.log(rules)
-          console.log(rules[0].rule + rules[0].text)
+          //rules and chapters, through parsing functions with their own regex's, separate relevant information from .text
+          setRules(parseRules(fetchData));
+          setChapters(parseChapters(fetchData));
         },
         (error) => {
           setIsLoaded(true);
           setError(error);
         }
       )
+      //After states are filled, mix the chapters and rules into a single list
+      .then(chaptersAndRules(chapters, rules))
   }, [])
 
-
+  const chaptersAndRules = (chapters, rules) => {
+    console.log("first chapter: " + chapters[0])
+    console.log("first rule: " + rules[0])
+    var mixed = [];
+  debugger
+    for (var i = 0; i < chapters.length; i++) {
+      mixed.push(chapters[i])
+  
+      for (var i2 = 0; i < rules.length; i2++){
+        console.log(rules[i2])
+        
+        if (chapters[i].number === rules[i2].rule.slice(0,3)) {
+          mixed.push(rules[i2])
+        }
+      }
+    }
+    console.log("logging mixed list:" + mixed)
+    debugger
+  }
+  
 
   //Possible returns
   if (error) {
@@ -55,12 +83,25 @@ function App() {
   } else {
     return (
       <div>
-        <p>Open console f12</p>
+        <p>Chapters</p>
+        <ul>
+          {chapters.map(item=> (
+            <li key={item.number}>
+              {item.number} {item.title}
+            </li>
+          ))}
+        </ul>
+        <p>Rules</p>
+        <ul>
+          {rules.map(item => (
+            <li key={item.rule}>
+              {item.rule} {item.text}
+            </li>
+          ))}
+        </ul>
       </div>
     )
   }
-
-
 }
 
 export default App;
